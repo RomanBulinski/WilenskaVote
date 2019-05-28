cb587c882514fd65d40fea3da1af4b024d6b386f
Roman Bulinski <aboandrus@gmail.com>
2019-05-28 09:17:49 +0200

    Rebuild structure of app

--- wvote/src/karma.conf.js
+++ wvote/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/wilenskavote'),
+      dir: require('path').join(__dirname, '../coverage/wvote'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
